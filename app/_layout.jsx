9e63b1783afe4ed9aeec8a2bb4d3ac475b15ebbc
Victor Yohanna <you@example.com>
2025-02-11 13:40:19 +0100

    add status bar

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,24 +1,28 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: true, title: "Home" }}
-      />
-      <Stack.Screen
-        name="onboarding"
-        options={{ headerShown: true, title: "Onboarding" }}
-      />
-      <Stack.Screen
-        name="landing"
-        options={{ headerShown: false, title: "" }}
-      />
-      <Stack.Screen
-        name="signup"
-        options={{ headerShown: false, title: "" }}
-      />
-    </Stack>
+    <>
+      <StatusBar style="auto" />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: true, title: "Home" }}
+        />
+        <Stack.Screen
+          name="onboarding"
+          options={{ headerShown: true, title: "Onboarding" }}
+        />
+        <Stack.Screen
+          name="landing"
+          options={{ headerShown: false, title: "" }}
+        />
+        <Stack.Screen
+          name="signup"
+          options={{ headerShown: false, title: "" }}
+        />
+      </Stack>
+    </>
   );
 }
--- app/landing.jsx
+++ app/landing.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 
+
 const LandingPage = () => {
   return (
     <SafeAreaView style={styles.container}>
